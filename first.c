683147abfabd32fe6c7cddc7607ae877453b097e
varmams-at-396364746856 <prssvraju2@gmail.com>
2022-09-14 17:49:24 +0530

    first commite

---
+++ first.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+float addArr(int[],int);
+int main()
+{
+    int i,n,a[10];
+    float avg;
+    printf("enter number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    avg=addArr(a,n);
+    printf("Average is %f",avg);
+
+    return 0;
+}
+float addArr(int a[],int n)
+{
+    int res,i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("Sum is %d\n",sum);
+    printf("N value is %d\n",n);
+    return sum/n;
+}
