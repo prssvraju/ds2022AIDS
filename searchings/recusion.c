aae2bb1ec282fcd572c576720636e9d4e403c3df
varmams-at-396364746856 <prssvraju2@gmail.com>
2022-09-15 16:19:32 +0530

    fact

---
+++ searchings/recusion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fact(int);
+int main()
+{
+    int n;
+    printf("Enter Number");
+    scanf("%d",&n);
+    printf("Fact of givne numbr %d is %d",n,fact(n));
+    return 0;
+}
+int fact(int num)
+{
+    if(num ==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num*fact(num-1);
+    }
+}
