b948d818b1f64d5f0f4a7f1622b6a345fd7055d7
varmams-at-396364746856 <prssvraju2@gmail.com>
2022-09-18 09:27:14 +0530

    bool update

--- Basics/fidserice.c
+++ Basics/fidserice.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+#ifndef __cplusplus
+typedef unsigned char bool;
+static const bool False = 0;
+static const bool True = 1;
+#endif
 int main()
 {
+    bool test;
     int f1=0,f2=1,f3=f1+f2;
     int i;
     printf("%d\n%d\n",f1,f2);
