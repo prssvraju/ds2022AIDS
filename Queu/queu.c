26b4b2b5c5a69ff49d18b1e810cf3aa6766bb208
raghu <prssvraju2@gmail.com>
2022-10-20 10:18:37 +0530

    updated queue

--- Queu/queu.c
+++ Queu/queu.c
@@ -7,13 +7,13 @@ struct  queue
     int front,rear;
 };
 void enqueue(struct queue*,int);
-int dequeue(struct queue*);
-int peek(struct queue*);
+void dequeue(struct queue*);
+//int peek(struct queue*);
 void display(struct queue*);
 int main()
 {
     int i;
-    int ch;
+    int ch,ele;
     struct queue qu;
     qu.front=0;
     qu.rear = -1;
@@ -30,14 +30,21 @@ int main()
         switch (ch)
         {
         case 1:
+            printf("Enter element to insert");
+            scanf("%d",&ele);
+            enqueue(&qu,ele);
             break;
         case 2:
+            dequeue(&qu);
             break;
         case 3:
+            display(&qu);
             break;
         case 4:
+            printf("Peek");
             break;
         case 5:
+            printf("Empty");
             break;
         case 6:
             exit(0);
@@ -62,4 +69,35 @@ void enqueue(struct queue *qu,int element)
         qu->rear = qu->rear+1;
         qu->items[qu->rear]=element;
     }
+}
+
+void dequeue(struct queue *qu)
+{
+    int element;
+    if(qu->rear<qu->front)
+    {
+      printf("Queue is under flow");   
+    }
+    else
+    {
+        element = qu->items[qu->front];
+        qu->front++;
+        printf("element remove is %d",element);
+    }
+}
+void display(struct queue *qu)
+{
+    int i;
+    if(qu->rear<qu->front)
+    {
+      printf("Queue is under flow");   
+    }
+    else
+    {
+        for(i=qu->front;i<=qu->rear;i++)
+        {
+         printf("\n%d indexed value is |%d|",i,qu->items[i]);
+        }
+    }
+
 }
