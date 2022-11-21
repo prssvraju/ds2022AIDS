448fdcd4db9b98378a2a1d97e041a32856fd9976
raghu <prssvraju2@gmail.com>
2022-11-21 10:01:48 +0530

    Poly Add code update

--- LinkedList/tempCodeRunnerFile.c
+++ LinkedList/tempCodeRunnerFile.c
@@ -1,1 +1,33 @@
-exp
+struct node *h1=ph1;
+    // struct node *h2=ph2;
+    // struct node *h3=NULL;
+    // while(h1!=NULL && h2 !=NULL)
+    // {
+    //     if(h1->exp == h2->exp)
+    //     {
+    //         h3 = insertPolyNode(h3,h1->cof+h2->cof,h1->exp);
+    //         h1 = h1->next;
+    //         h2 = h2->next;
+    //     }
+    //     if(h1->exp > h2->exp)
+    //     {
+    //         h3 = insertPolyNode(h3,h1->cof,h1->exp);
+    //         h1 = h1->next;
+    //     }
+    //     if(h1->exp < h2->exp)
+    //     {
+    //         h3 = insertPolyNode(h3,h2->cof,h2->exp);
+    //         h2 = h2->next;
+    //     }
+    // }
+    // while (h1!=NULL)
+    // {
+    //     h3 = insertPolyNode(h3,h1->cof,h1->exp);
+    //     h1 = h1->next;
+    // }
+    // while (h2!=NULL)
+    // {
+    //     h3 = insertPolyNode(h3,h2->cof,h2->exp);
+    //     h2 = h2->next;
+    // }
+    // return h3;
